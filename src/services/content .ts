dc934cc14089e9df0e3b296fac92eea3d0cc3861
Novfal-202 <novfal@navdhaiti.com>
2024-09-23 17:40:28 +0530

    methods name correction done.

--- src/services/content .ts
+++ src/services/content .ts
@@ -1,6 +1,5 @@
-import { Content } from '../models/content'; // Import Content model
+import { Content } from '../models/content';
 
-// Create a new content
 export const createContent = async (insertData: Record<string, unknown>[]): Promise<any> => {
   const insertContent = await Content.bulkCreate(insertData);
   return { insertContent };
